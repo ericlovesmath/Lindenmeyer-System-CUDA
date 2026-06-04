64c1c5fcd4d6dedf4818074cb17bd1325b7dc35b
Eric Lee <me@ericchanlee.com>
2026-06-04 11:11:35 -0700

    feat: nix flake to use all actually necessary packages for full hosting

--- flake.nix
+++ flake.nix
@@ -11,17 +11,33 @@
         inherit system;
         config.allowUnfree = true;
       };
+      cuda = pkgs.cudaPackages;
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
+      devShells.${system}.default = (pkgs.mkShell.override { stdenv = cuda.backendStdenv; }) {
         packages = with pkgs; [
           cmake
           ninja
-          gcc
+          git
           clang-tools
-          cudaPackages.cuda_nvcc
-          cudaPackages.cuda_cudart
+          virtualgl # vglrun to run GL to the real GPU over VNC
+
+          cuda.cuda_nvcc
+          cuda.cuda_cudart
+          cuda.cuda_cccl # Thrust
+          cuda.cuda_nvtx
+          cuda.nsight_compute
+
+          glfw
+          libGL
         ];
+
+        CUDA_PATH = cuda.cudatoolkit; # find_package(CUDAToolkit)
+
+        # libcuda.so ships with the host driver, not nixpkgs
+        shellHook = ''
+          export LD_LIBRARY_PATH="/run/opengl-driver/lib:/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+        '';
       };
     };
 }
